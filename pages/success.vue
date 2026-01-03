f1d2672ef43bb8166a1aed570f5de357755b9a99
fnafpizza <fnafnoreply@gmail.com>
2026-01-02 23:35:22 -0700

    Fix build error: add lang='ts' to success.vue script tag

--- pages/success.vue
+++ pages/success.vue
@@ -73,7 +73,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useCartStore } from '~/stores/cart'
